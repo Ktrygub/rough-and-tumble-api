256f5b960c141dbc9edf15c4ab99a249d4716d5d
Ktrygub <konstantin.trygub@gmail.com>
2018-01-19 22:04:31 +0200

    Mongoose User model, auth route

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import path from 'path'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+import auth from './routes/auth'
+
+dotenv.config()
+
+const app = express()
+
+mongoose.connect(process.env.MONGODB_URL)
+
+app.use(bodyParser.json())
+
+app.use('/api/auth', auth)
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen(8080, () => console.log('Running on localhost:8080'))
